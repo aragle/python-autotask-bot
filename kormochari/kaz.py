36fd0da04d5bf4af21c27bde8d0477ff075e08c5
Ashiqur Rahman Alif <62181222+aratheunseen@users.noreply.github.com>
2022-10-17 03:02:11 +0600

    update

---
+++ kormochari/kaz.py
@@ -0,0 +1,34 @@
+import pyautogui
+
+
+def amar_jaiga_dekhao(n_second_pore=1):
+    pyautogui.time.sleep(n_second_pore)
+    print(pyautogui.position())
+
+
+def jao(x, y, n_second_pore):
+    pyautogui.moveTo(x, y, duration=n_second_pore)
+
+
+def lekho(kotha):
+    pyautogui.typewrite(kotha)
+
+
+def chapo():
+    pyautogui.click()
+
+
+def dan_button_chapo():
+    pyautogui.rightClick()
+
+
+def bam_button_chapo():
+    pyautogui.leftClick()
+
+
+def button_chapo(button):
+    pyautogui.press(button)
+
+
+def dui_bar_chapo():
+    pyautogui.doubleClick()
